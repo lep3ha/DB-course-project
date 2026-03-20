bc92dd23958c3046547ba0c02c616d06ee8a3329
Alexander Youguy <alexandr.youguy@gmail.com>
2026-03-21 01:33:56 +0300

    final UPD

--- app/cart/forms.py
+++ app/cart/forms.py
@@ -3,13 +3,6 @@ from wtforms import HiddenField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
 
-class AddToCartForm(FlaskForm):
-    workpiece_id = HiddenField('workpiece_id', validators=[DataRequired()])
-    price_at_add = HiddenField('price_at_add', validators=[DataRequired()])
-    quantity = IntegerField('quantity', default=1, validators=[DataRequired(), NumberRange(min=1)])
-    submit = SubmitField('Добавить в корзину')
-
-
 class UpdateCartItemForm(FlaskForm):
     item_id = HiddenField('item_id', validators=[DataRequired()])
     quantity = IntegerField('quantity', validators=[DataRequired(), NumberRange(min=1)])
